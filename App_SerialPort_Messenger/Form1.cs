2ec64a39b3f7528b71e671b4948bff1ad9483d55
Ventthos <ferra04@live.com.mx>
2024-09-29 01:01:42 -0600

    feat: write in txt
    Ya escribe lo que se recibe en el puerto serial en un txt

--- App_SerialPort_Messenger/Form1.cs
+++ App_SerialPort_Messenger/Form1.cs
@@ -12,11 +12,14 @@ using System.Security.Policy;
 using MaterialSkin;
 using MaterialSkin.Controls;
 using System.IO;
+using App_SerialPort_Messenger.bin.Debug.Logs;
+using System.IO.Ports;
 
 namespace App_SerialPort_Messenger
 {
     public partial class Main_Dashboard : MaterialForm
     {
+        private SerialPort serialPort;
 
         public Main_Dashboard()
         {
@@ -40,9 +43,20 @@ namespace App_SerialPort_Messenger
                 c.Font = new Font(pfc.Families[0], c.Font.Size, c.Font.Style);
             }
 
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 115200;
+            serialPort.PortName = "COM5";
+            serialPort.Open();
+            Message_Receiver_manager messageManager = new Message_Receiver_manager(serialPort);
+
         }
 
         private void UsersList_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TxtBox_Message_Click(object sender, EventArgs e)
         {
 
         }
---
+++ App_SerialPort_Messenger/Message_Receiver_manager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.IO;
+using System.Windows.Forms;
+
+namespace App_SerialPort_Messenger.bin.Debug.Logs
+{
+
+    public class Message_Receiver_manager
+    {
+        // El puerto serial
+        private SerialPort serialPort;
+
+        // Encapsulación
+        public SerialPort SerialPortAssigned { get { return serialPort; } private set { serialPort = value; } }
+
+        //Donde estarán los mensajes que recibe
+        string receivedData;
+        
+        // Recibimos puerto serial, para compartir ya una misma instancia
+        public Message_Receiver_manager(SerialPort serialPort)
+        {
+            SerialPortAssigned = serialPort;
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            MessageBox.Show(Path.GetDirectoryName(Application.ExecutablePath));
+        }
+
+        public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                try
+                {
+                    receivedData = serialPort.ReadLine();
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Error al leer datos: " + ex.Message);
+                }
+
+                string user = GetUser(receivedData);
+                WriteInTxt(receivedData, user);
+            }
+        }
+
+        private string GetUser(string message) {
+            int startIndex = message.IndexOf("[") + 1;
+            int endIndex = message.IndexOf(':');
+
+            return message.Substring(startIndex , endIndex - startIndex);
+        }
+
+        private void WriteInTxt(string message, string user)
+        {
+            string route = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Logs", $"Log_{user}.txt");
+            using (StreamWriter sw = new StreamWriter(route, true))
+            {
+                sw.WriteLine(message);
+            }
+        }
+    }
+
+
+    
+}
